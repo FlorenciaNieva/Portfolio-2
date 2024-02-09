a3abf87d4dc0f5d71a7776aba77bc89ae720b367
FlorenciaNieva <florencianieva930@gmail.com>
2024-02-08 22:07:46 -0300

    Agrego rutas con sus componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutMe from "./components/AboutMe/AboutMe";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<AboutMe />}/>
+          <Route path="/projects" element={<Projects />}/>
+          <Route path="/contact" element={<Contact />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App;
