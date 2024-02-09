6511bbd83f2b0d6f9b97dcfbcfd90b5bf7daa4a3
FlorenciaNieva <florencianieva930@gmail.com>
2024-02-09 20:39:47 -0300

    Traigo la info de firestore y renderizo el carusel circular pasandole la info

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,31 @@
-import 'react-fancy-circular-carousel/FancyCarousel.css';
+import React, { useEffect, useState } from "react";
+import CircularCarousel from "../CircularCarousel/CircularCarousel";
+import { collection, getDocs } from "firebase/firestore";
+import db from "../../../firestore.config";
 
 export default function Projects() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const collec = collection(db, "projects");
+        const docs = await getDocs(collec);
+        const documents = docs.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setData(documents);
+      } catch (error) {
+        console.error("Error fetching data from Firestore:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div>Projects</div>
-  )
+    <>
+      <CircularCarousel data={data} />
+    </>
+  );
 }
