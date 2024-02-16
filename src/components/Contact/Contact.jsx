ceef5ff806ed56748b56d45f762688f9d0611c2a
FlorenciaNieva <florencianieva930@gmail.com>
2024-02-16 16:47:17 -0300

    Agrego formulario a componente Contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Container from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import FormContact from "../FormContact/FormContact";
 
 export default function Contact() {
   return (
     <Container fluid className="container-contact">
       <Container className="d-flex flex-column">
         <h2 className="text-gradient mt-5 mb-5">CONTACT ME</h2>
+        <FormContact />
       </Container>
     </Container>
   );
