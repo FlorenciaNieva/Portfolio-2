56d20e4bf7286e50fb1190cb3a16aeae8366ec3e
FlorenciaNieva <florencianieva930@gmail.com>
2024-02-16 16:41:42 -0300

    Creo formulario de contacto

--- src/components/FormContact/FormContact.jsx
+++ src/components/FormContact/FormContact.jsx
@@ -1,7 +1,69 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "@formspree/react";
+import { Form, FloatingLabel, Button } from "react-bootstrap";
 
 export default function FormContact() {
+  const [state, handleSubmit] = useForm("xdoqglvr");
+
+  if (state.succeeded) {
+    return <p>Thanks for communicating with me!</p>;
+  }
+
   return (
-    <div>FormContact</div>
-  )
+    <form className="d-flex flex-column" onSubmit={handleSubmit}>
+      <FloatingLabel
+        data-bs-theme="dark"
+        label="Name"
+        className="floating-label mb-3"
+      >
+        <Form.Control
+          type="text"
+          name="name"
+          placeholder="Name"
+          className="form-control"
+          id="name"
+          autocomplete="off"
+        />
+      </FloatingLabel>
+      <FloatingLabel
+        data-bs-theme="dark"
+        label="Email"
+        className="floating-label mb-3"
+      >
+        <Form.Control
+          type="email"
+          name="email"
+          placeholder="Email"
+          className="form-control"
+          id="email"
+          autocomplete="off"
+        />
+      </FloatingLabel>
+      <FloatingLabel
+        data-bs-theme="dark"
+        label="Message"
+        className="floating-label"
+      >
+        <Form.Control
+          as="textarea"
+          type="textarea"
+          name="message"
+          placeholder="Message"
+          style={{ height: "100px" }}
+          className="form-control"
+          id="message"
+        />
+      </FloatingLabel>
+      <div>
+        <Button
+          type="submit"
+          disabled={state.submitting}
+          className="button-animation m-3"
+        >
+          <span className="button-text">Send Message</span>
+          <div className="fill-container"></div>
+        </Button>
+      </div>
+    </form>
+  );
 }
