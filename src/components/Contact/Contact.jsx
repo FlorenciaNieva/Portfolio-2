53b5405d5fd7ea18ce71673803e4a57206d4e2b7
FlorenciaNieva <florencianieva930@gmail.com>
2024-02-19 18:30:57 -0300

    Centro el formulario y el titulo

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import FormContact from "../FormContact/FormContact";
+import { Row, Col } from "react-bootstrap";
 
 export default function Contact({ menuOpen }) {
   return (
     <Container fluid className="container-contact">
       <Container className="d-flex flex-column pt-5">
-        <Container className={menuOpen ? "menu-open" : "menu-close"}>
-          <h2 className="text-gradient mt-5 mb-3">CONTACT ME</h2>
-          <FormContact />
+        <Container className={`${menuOpen ? "menu-open" : "menu-close"}`}>
+          <h2 className="text-center text-gradient mt-5 mb-3">CONTACT ME</h2>
+          <Row className="justify-content-center">
+            <Col lg="9">
+              <FormContact />
+            </Col>
+          </Row>
         </Container>
       </Container>
     </Container>
