5a134aaec380497dad50ae20e005fd5f84a2555c
FlorenciaNieva <florencianieva930@gmail.com>
2024-02-19 14:46:42 -0300

    Modifico los estilos y el contenido del home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,19 +7,14 @@ export default function Home() {
   return (
     <>
       <Container fluid className="container-home-background">
-        <Container>
+        <Container className="container-home">
           <Row>
             <Col>
-              <h3 className="sub-title-home">Hi! I'm</h3>
+              <h3 className="subtitle-home">Hi! My name is</h3>
               <Col className="d-flex text-animation">
-                <h1>Flor Nieva</h1>
+                <h1>Florencia Nieva</h1>
               </Col>
-              <h3
-                className="sub-title-home"
-                style={{ fontFamily: "Dejavu Sans" }}
-              >
-                Frontend Developer
-              </h3>
+              <h3 className="description-home">Frontend Developer</h3>
               <Col>
                 <Button className="button-animation" href="#skills">
                   <span className="button-text">View Projects</span>
