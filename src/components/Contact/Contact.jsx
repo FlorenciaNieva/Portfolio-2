777c07b37f28a48fe43367bd31e5de20373c7ba1
FlorenciaNieva <florencianieva930@gmail.com>
2024-02-19 18:31:23 -0300

    Agrego parrafo antes del formulario

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,6 +9,7 @@ export default function Contact({ menuOpen }) {
       <Container className="d-flex flex-column pt-5">
         <Container className={`${menuOpen ? "menu-open" : "menu-close"}`}>
           <h2 className="text-center text-gradient mt-5 mb-3">CONTACT ME</h2>
+          <p className=" text-center mb-5">Feel free to Contact me by submitting the form below and I will get back to you as soon as possible.</p>
           <Row className="justify-content-center">
             <Col lg="9">
               <FormContact />
