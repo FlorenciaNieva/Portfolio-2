d6f8d9445fcbfc91c89494ff79827c0a4542f56a
FlorenciaNieva <florencianieva930@gmail.com>
2024-02-19 11:55:55 -0300

    Creo contenido del footer con sus estilos

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import Image from "react-bootstrap/Image";
+import Logo from "../../assets/logo/logo.svg";
+import { Container, Nav } from "react-bootstrap";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { MdOutlineMailOutline } from "react-icons/md";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="text-center pt-5 pb-3">
+      <Container>
+        <Image src={Logo} alt="logo" className="logo-footer mb-4" />
+        <Nav className="d-flex justify-content-center align-items-center">
+          <Nav.Link href="https://github.com/FlorenciaNieva">
+            <FaGithub fontSize="45px" className="icon-social" />
+          </Nav.Link>
+          <Nav.Link href="https://www.linkedin.com/in/florencia-nievaa/">
+            <FaLinkedin fontSize="45px" className="icon-social" />
+          </Nav.Link>
+          <Nav.Link href="mailto:florencianieva930@gmail.com">
+            <MdOutlineMailOutline fontSize="45px" className="icon-social" />
+          </Nav.Link>
+        </Nav>
+        <p className="mt-3">Made by Florencia Nieva</p>
+      </Container>
+    </footer>
+  );
 }
