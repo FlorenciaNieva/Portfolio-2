e99895da77bff781891f493465b31a3399acf663
FlorenciaNieva <florencianieva930@gmail.com>
2024-02-20 21:35:00 -0300

    Agrego función handleClick con navigate y window.scrollTo

--- src/components/AnimatedButton/AnimatedButton.jsx
+++ src/components/AnimatedButton/AnimatedButton.jsx
@@ -6,8 +6,13 @@ import { GoArrowRight } from "react-icons/go";
 export default function AnimatedButton({ route, content }) {
   const navigate = useNavigate();
 
+  const handleClick = () => {
+    navigate(route);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
-    <Button className="button-animation mt-2" onClick={() => navigate(route)}>
+    <Button className="button-animation mt-2" onClick={() => handleClick()}>
       <span className="button-text">
         {content}
         <GoArrowRight className="arrow-animation ml-2" fontSize="25px" />{" "}
