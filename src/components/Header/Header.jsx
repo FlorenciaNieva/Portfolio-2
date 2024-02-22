8b31b3bd9d1470b93b4b515e39ff643baa003ec8
FlorenciaNieva <florencianieva930@gmail.com>
2024-02-22 20:07:03 -0300

    Agrego aria-label a los iconos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -41,21 +41,21 @@ export default function Header({ handleToggleMenu }) {
               target="_blank"
               className="mx-2"
             >
-              <FaGithub size="25px" />
+              <FaGithub size="25px" aria-label="GitHub" />
             </a>
             <a
               href="https://www.linkedin.com/in/florencia-nievaa/"
               target="_blank"
               className="mx-2"
             >
-              <FaLinkedin size="25px" />
+              <FaLinkedin size="25px" aria-label="Linkedin" />
             </a>
             <a
               href="mailto:florencianieva930@gmail.com"
               target="_blank"
               className="mx-2"
             >
-              <MdOutlineMailOutline size="25px" />
+              <MdOutlineMailOutline size="25px" aria-label="Email" />
             </a>
           </div>
         </Navbar.Collapse>
