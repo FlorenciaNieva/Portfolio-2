2a520f5ac8015f9b15d35e2544cd0c4c791853b4
FlorenciaNieva <florencianieva930@gmail.com>
2024-02-22 19:52:19 -0300

    Elimino clases del header y agrego redes sociales a el header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,12 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo/logo.svg";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Header({ handleToggleMenu }) {
   return (
-    <header className="mt-2 px-5">
-      <Navbar data-bs-theme="dark" expand="sm" className="mx-5">
+    <header>
+      <Navbar data-bs-theme="dark" expand="sm" className="mx-4 mt-2">
         <img
           alt="logo"
           src={logo}
@@ -33,6 +34,18 @@ export default function Header({ handleToggleMenu }) {
               Contact
             </Link>
           </Nav>
+          <div className="d-flex justify-content-center ms-auto mt-2">
+            <a href="https://github.com/FlorenciaNieva" target="_blank">
+              <FaGithub size="25px" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/florencia-nievaa/"
+              target="_blank"
+              className="mx-3"
+            >
+              <FaLinkedin size="25px" />
+            </a>
+          </div>
         </Navbar.Collapse>
       </Navbar>
     </header>
