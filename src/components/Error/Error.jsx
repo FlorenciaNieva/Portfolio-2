cf71bf796801e45cf9a12918b4a32e3ea3cea6d8
FlorenciaNieva <florencianieva930@gmail.com>
2024-03-11 17:28:40 -0300

    Agrego Contenido dentro del componente Error

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
+import { MdErrorOutline } from "react-icons/md";
 
 export default function Error() {
   return (
-    <div>Error</div>
-  )
+    <Container
+      fluid
+      className="background d-flex align-items-center min-vh-100 pt-5"
+    >
+      <Container className="pt-5 text-center">
+      <MdErrorOutline fontSize="50px" />
+        <h1>The page could not be found</h1>
+      </Container>
+    </Container>
+  );
 }
