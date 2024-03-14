05a40305f5fcf7679e22257b6660452c8c91b552
FlorenciaNieva <florencianieva930@gmail.com>
2024-03-14 15:49:30 -0300

    Modifico los margenes de los componentes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,10 +11,10 @@ export default function Contact({ menuOpen }) {
     <Container fluid className="container-contact pb-5">
       <Container className="d-flex flex-column pt-5 text-center">
         <Container className={`${menuOpen ? "menu-open" : "menu-close"}`}>
-          <h2 className="letter-color mt-5 mb-3">
+          <h2 className="letter-color my-5">
             {isEnglish ? ('CONTACT ME') : ('CONTACTAME')}
           </h2>
-          <p className="my-4">
+          <p>
             {isEnglish 
             ? ('Feel free to Contact me by submitting the form below and I will get back to you as soon as possible.')
             : ('No dudes en ponerte en contacto conmigo enviando el siguiente formulario y me comunicaré con usted lo antes posible.')}
@@ -22,7 +22,7 @@ export default function Contact({ menuOpen }) {
           <Row className="justify-content-center">
             <Col lg="9">
               <FormContact />
-              <div className="mt-5">
+              <div className="my-5">
                 <ArrowButton route="/" content={isEnglish ? ('Go Back Home') : ('Volver a Inicio')} />
               </div>
             </Col>
