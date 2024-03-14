d88eb7174344f7bb72038cba6a03ad9cba9f6e9b
FlorenciaNieva <florencianieva930@gmail.com>
2024-03-14 14:04:18 -0300

    Importo al header funciones del contexto useLanguage y agrego toggle para modificar el idioma de la página

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,11 @@ import { Nav, Navbar, Image, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo/logo.svg";
 import { IoCloseSharp } from "react-icons/io5";
+import { useLanguage } from "../../context/LanguagesContext";
 
 export default function Header({ handleToggleMenu, menuOpen }) {
+  const { isEnglish, toggleLanguage } = useLanguage();
+
   return (
     <header className="w-100 position-absolute">
       <Container>
@@ -25,18 +28,23 @@ export default function Header({ handleToggleMenu, menuOpen }) {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mx-3">
               <Link to="/" className="link text-center">
-                Home
+                {isEnglish ? ('Home') : ('Inicio')}
               </Link>
               <Link to="/about-me" className="link text-center">
-                About me
+                {isEnglish ? ('About me') : ('Sobre mi')}
               </Link>
               <Link to="/projects" className="link text-center">
-                Projects
+                {isEnglish ? ('Projects') : ('Proyectos')}
               </Link>
               <Link to="/contact" className="link text-center">
-                Contact
+                {isEnglish ? ('Contact') : ('Contacto')}
               </Link>
             </Nav>
+            <div className="d-flex justify-content-center ms-auto mt-2">
+              <a onClick={toggleLanguage}>
+                {isEnglish ? "EN" : "ES"}
+              </a>
+            </div>
           </Navbar.Collapse>
         </Navbar>
       </Container>
