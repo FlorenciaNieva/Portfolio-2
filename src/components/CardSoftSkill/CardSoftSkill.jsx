582cfcffc72a40267c9d785cc4886322498b320d
FlorenciaNieva <florencianieva930@gmail.com>
2024-04-01 17:49:10 -0300

    Modifico el código de la tarjeta para hacerla reutilizable

--- src/components/CardSoftSkill/CardSoftSkill.jsx
+++ src/components/CardSoftSkill/CardSoftSkill.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 import { Card, Col } from "react-bootstrap";
-import { FaAssistiveListeningSystems, FaBusinessTime } from "react-icons/fa";
-import { RiTeamFill } from "react-icons/ri";
-import { IoBook } from "react-icons/io5";
-import { useLanguage } from "../../context/LanguagesContext";
 
-export default function CardSoftSkill({ titleEn, titleEs }) {
-  const { isEnglish } = useLanguage();
-
+export default function CardSoftSkill({ title, icon }) {
   return (
     <Col
       className="d-flex justify-content-center mb-5"
@@ -16,18 +10,10 @@ export default function CardSoftSkill({ titleEn, titleEs }) {
     >
       <Card className="d-flex flex-column card-soft-skill text-center position-relative">
         <div className="position-absolute soft-skill-icon">
-          {titleEn === "Teamwork" ? (
-            <RiTeamFill fontSize="5rem" />
-          ) : titleEn === "Time Management" ? (
-            <FaBusinessTime fontSize="5rem" />
-          ) : titleEn === "Love Of Learning" ? (
-            <IoBook fontSize="5rem" />
-          ) : titleEn === "Active Listening" ? (
-            <FaAssistiveListeningSystems fontSize="5rem" />
-          ) : null}
+          {icon}
         </div>
         <Card.Text className="mt-5 pt-4">
-          {isEnglish ? titleEn : titleEs}
+          {title}
         </Card.Text>
       </Card>
     </Col>
